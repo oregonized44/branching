5325d137afb32e3e4aa95f71de1a68445da28124
Connor Cummings and John Klein <connor@>
2016-03-24 10:40:16 -0700

    final functionality

--- js/script.js
+++ js/script.js
@@ -10,16 +10,23 @@ $(document).ready(function(){
     var lastName = $("#lastName").val();
     var age = $("#age").val();
     var party = $("input:radio[name=party]:checked").val();
+    var fullName = firstName + " " + lastName;
+
+    $(".name").text(fullName);
 
     switch (party) {
       case "democrat":
-        console.log("dem");
+        $(".link").remove();
+        $(".site").append('<a class="link"  href="http://www.msnbc.com/">MSNBC</a>');
         break;
+        console.log(dem);
       case "republican":
-        console.log("rep");
+        $(".link").remove();
+        $(".site").append('<a class="link" href="http://www.foxnews.com/">Fox News</a>');
         break;
       case "independant":
-        console.log("ind");
+        $(".link").remove();
+        $(".site").append('<a class="link"  href="http://www.independent.co.uk/">The Independent</a>');
         break;
       default:
         alert("Please select a party");
@@ -27,7 +34,7 @@ $(document).ready(function(){
     // push sentence
     console.log(age);
     if (age > 18 ) {
-
+      $(".results").show();
     } else {
       alert("You must be atleast 18 years old to participate!");
       console.log("underage");
