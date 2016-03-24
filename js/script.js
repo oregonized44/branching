642fde6f98f11db9c4d3df3831e1d96099659224
Connor Cummings and John Klein <john@>
2016-03-24 09:38:39 -0700

    Add page content and structure. Add functionality

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+//initializing our query click function
+$(document).ready(function(){
+  $(".btn").click(function(){
+
+//setting our variables
+
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var age = parseInt($("#age").val());
+    var party = $("")
+
+
+
+  });
+});
